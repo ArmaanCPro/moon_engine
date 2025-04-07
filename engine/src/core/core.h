95f1c6194f5d7c69f820d5f82c2442a8a7792eea
Armaan Chahal <armaan.c2009@gmail.com>
2025-04-07 18:12:43 -0500

    Add cross-platform debug break macros
    Implemented `MOON_DEBUGBREAK()` for cross-platform support of debug breaks, covering MSVC, MinGW, Clang, and GCC. Updated assertion macros to use `MOON_DEBUGBREAK()` instead of platform-specific debug break calls.

--- engine/src/core/core.h
+++ engine/src/core/core.h
@@ -14,12 +14,36 @@
     #endif
 #endif
 
+// Debug break cross-platform implementation
+#ifdef DEBUG
+    #if defined(_MSC_VER)
+        #define MOON_DEBUGBREAK() __debugbreak()
+    #elif defined(__MINGW32__) || defined(__MINGW64__)
+        // For MinGW, use SIGBREAK which is always defined
+        #define MOON_DEBUGBREAK() raise(SIGBREAK)
+    #elif defined(__clang__)
+        #if defined(__has_builtin) && __has_builtin(__builtin_debugtrap)
+            #define MOON_DEBUGBREAK() __builtin_debugtrap()
+        #else
+            #include <signal.h>
+            #define MOON_DEBUGBREAK() raise(SIGTRAP)
+        #endif
+    #elif defined(__GNUC__)
+        #include <signal.h>
+        #define MOON_DEBUGBREAK() raise(SIGTRAP)
+    #else
+        #error "Platform doesn't support debugbreak yet!"
+    #endif
+#endif
+
+// assertions
 #ifdef MOON_ENABLE_ASSERTS
-    #define MOON_ASSERT(x, ...) { if(!(x)) { MOON_ERROR("Assertion Failed: {0} ({1}:{2} in function {3})", __VA_ARGS__, __FILE__, __LINE__, __FUNCTION__); __debugbreak(); } }
-    #define MOON_CORE_ASSERT(x, ...) { if(!(x)) { MOON_CORE_ERROR("Assertion Failed: {0} ({1}:{2})", __VA_ARGS__, __FILE__, __LINE__); __debugbreak(); } }
+    #define MOON_ASSERT(x, ...) { if(!(x)) { MOON_ERROR("Assertion Failed at {}:{}: {}", __FILE__, __LINE__, __VA_ARGS__); MOON_DEBUGBREAK(); } }
+    #define MOON_CORE_ASSERT(x, ...) { if(!(x)) { MOON_CORE_ERROR("Assertion Failed at {}:{}: {}", __FILE__, __LINE__, __VA_ARGS__); MOON_DEBUGBREAK(); } }
 #else
     #define MOON_ASSERT(x, ...)
     #define MOON_CORE_ASSERT(x, ...)
 #endif
+
 
 #define BIT(x) (1 << x)

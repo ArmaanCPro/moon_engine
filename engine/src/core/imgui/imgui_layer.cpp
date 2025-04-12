5a37fc219d723f93f5a29d93e6f40d14602d61b0
Armaan Chahal <armaan.c2009@gmail.com>
2025-04-11 22:17:53 -0500

    Enable ImGui docking and viewport config flags.

--- engine/src/core/imgui/imgui_layer.cpp
+++ engine/src/core/imgui/imgui_layer.cpp
@@ -31,6 +31,8 @@ namespace moon
 
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
         io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
         io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
 

93064ed4b51ccada30bc1bed145b95df2be7b2ab
Armaan Chahal <armaan.c2009@gmail.com>
2025-04-14 19:52:07 -0500

    Messing around with a circle shader using a simple SDF

--- sandbox/assets/shaders/flat_color.glsl
+++ sandbox/assets/shaders/flat_color.glsl
@@ -5,8 +5,11 @@ layout (location = 0) in vec3 a_Pos;
 uniform mat4 u_VP = mat4(1.0);
 uniform mat4 u_Model = mat4(1.0);
 
+out vec3 v_Pos;
+
 void main()
 {
+    v_Pos = a_Pos;
     gl_Position = u_VP * u_Model * vec4(a_Pos, 1.0);
 }
 
@@ -17,7 +20,13 @@ layout(location = 0) out vec4 FragColor;
 
 uniform vec4 u_Color;
 
+in vec3 v_Pos;
+
 void main()
 {
-    FragColor = u_Color;
+    vec3 circle = vec3(v_Pos.x / 2.0, v_Pos.y / 2.0, 0.1);
+    float d = length(v_Pos.xy - circle.xy) - circle.z;
+    d = smoothstep(0.0, 0.001, d);
+
+    FragColor = u_Color * (1 - d);
 }
